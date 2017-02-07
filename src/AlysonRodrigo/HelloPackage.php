45a1e112de3d61a97baaa57b9080d6e09538e603
Alyson Rodrigo <g.dasilva@hotmail.com>
2017-02-07 00:54:42 -0300

    Lib teste

---
+++ src/AlysonRodrigo/HelloPackage.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alyson
+ * Date: 07/02/17
+ * Time: 00:02
+ */
+
+namespace AlysonRodrigo;
+
+
+class HelloPackage
+{
+
+    public function getHello(){
+        return "Ola mundo";
+    }
+}
